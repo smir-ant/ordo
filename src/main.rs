0a9cccf6129e5194bd1aef7500695e28ef8bc40f
smir-ant <abcsmir.ant@yandex.ru>
2026-01-16 15:10:13 +0700

    hello world

---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use makepad_widgets::*;
+
+live_design!{
+    use makepad_widgets::window::Window;
+    use makepad_widgets::label::Label;
+    use makepad_widgets::view_ui::View;
+    use makepad_widgets::theme_desktop_dark::*; 
+    
+    App = {{App}} {
+        ui: <Window> {
+            body = <View> {
+                flow: Down,
+                align: {x: 0.5, y: 0.5},
+                show_bg: true,
+                draw_bg: {
+                    color: #1f1f1f
+                }
+                
+                <Label> {
+                    text: "Hello, World!",
+                    draw_text: {
+                        color: #ffffff,
+                        text_style: {font_size: 32.0}
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Live, LiveHook)]
+pub struct App {
+    #[live] ui: WidgetRef,
+}
+
+impl LiveRegister for App {
+    fn live_register(cx: &mut Cx) {
+        makepad_widgets::live_design(cx);
+    }
+}
+
+impl AppMain for App {
+    fn handle_event(&mut self, cx: &mut Cx, event: &Event) {
+        self.ui.handle_event(cx, event, &mut Scope::empty());
+    }
+}
+
+app_main!(App);
+
+fn main() {
+    app_main();
+}
+
